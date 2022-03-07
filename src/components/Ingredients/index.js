152693dddad51bfdfd0b013123e376fcf11fd9bb
shoufy <ducksophie@hotmail.fr>
2022-03-07 10:50:51 +0100

    play with var scss - ingredients components

--- src/components/Ingredients/index.js
+++ src/components/Ingredients/index.js
@@ -4,22 +4,23 @@ import PropTypes from 'prop-types';
 
 // == Import
 import './styles.scss';
+import 'src/styles/index.scss';
 
 // == Composant
 const Ingredients = ({ ingredients }) => {
   return (
     <div className="ingredients">
-      <div> Ingrédients :</div>  
+      <h2 className="ingredients-title"> Ingrédients :</h2>
       <ul>
         {
           // Je reçois des objets dans le tableau "ingrédients"
           // que je déstructure pour récupérer 
           // les propriétés qui m' intéressent
           ingredients.map(({ id, quantity, unit, name }) => (
-            <li key={id}>
-              <span>
+            <li key={id} className="ingredient">
+              <span className="ingredient-quantity">
                 {quantity} {unit}
-              </span>
+              </span >
               {name}
             </li>
           ))
